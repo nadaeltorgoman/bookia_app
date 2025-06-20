714b5c60d3db4c82550e9c6115a0e5144075022b
nadaeltorgoman <nada.khalid.eltorgoman@gmail.com>
2025-06-21 01:27:33 +0300

    handling constants

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,28 @@
+class AppAssets {
+  // paths
+  static const String imagesPath = 'assets/images/';
+  static const String iconsPath = 'assets/icons/';
+
+  // images
+  static const String logo = '${imagesPath}logo_splash.svg';
+  static const String background = '${imagesPath}background.png';
+  static const String userPlaceholder = '${imagesPath}user_placeholder.png';
+  static const String successMark = '${imagesPath}success_mark.svg';
+
+  // icons
+  static const String appleIcon = '${iconsPath}apple_icon.svg';
+  static const String googleIcon = '${iconsPath}google_icon.svg';
+  static const String facebookIcon = '${iconsPath}facebook_icon.svg';
+  static const String eyeIcon = '${iconsPath}eye_icon.svg';
+  static const String backArrowIcon = '${iconsPath}back_arrow_icon.svg';
+  static const String searchIcon = '${iconsPath}search_icon.svg';
+  static const String circleCloseIcon = '${iconsPath}circle_close_icon.svg';
+  static const String logoutIcon = '${iconsPath}logout_icon.svg';
+  static const String cameraIcon = '${iconsPath}camera_icon.svg';
+
+  // navigation bar icons
+  static const String homeIcon = '${iconsPath}Home.svg';
+  static const String profileIcon = '${iconsPath}Profile.svg';
+  static const String bookmarkIcon = '${iconsPath}Bookmark.svg';
+  static const String categoryIcon = '${iconsPath}Category.svg';
+}
---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,38 @@
+import 'package:bookia/core/utils/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static TextStyle getHeadline1({double? fontSize, FontWeight? fontWeight, Color? color}) =>
+      TextStyle(
+        fontSize: fontSize ?? 30,
+        fontWeight: fontWeight ?? FontWeight.normal,
+        color: color ?? AppColors.darkColor,
+      );
+
+  static TextStyle getHeadline2({double? fontSize, FontWeight? fontWeight, Color? color}) =>
+      TextStyle(
+        fontSize: fontSize ?? 24,
+        fontWeight: fontWeight ?? FontWeight.normal,
+        color: color ?? AppColors.darkColor,
+      );
+
+  static TextStyle getTitleText({double? fontSize, FontWeight? fontWeight, Color? color}) =>
+      TextStyle(
+        fontSize: fontSize ?? 18,
+        fontWeight: fontWeight ?? FontWeight.normal,
+        color: color ?? AppColors.darkColor,
+      );
+
+  static TextStyle getSmallText({double? fontSize, FontWeight? fontWeight, Color? color}) =>
+      TextStyle(
+        fontSize: fontSize ?? 16,
+        fontWeight: fontWeight ?? FontWeight.normal,
+        color: color ?? AppColors.greyColor,
+      );
+
+  static TextStyle getButton({double? fontSize, FontWeight? fontWeight, Color? color}) => TextStyle(
+    fontSize: fontSize ?? 14,
+    fontWeight: fontWeight ?? FontWeight.bold,
+    color: color ?? AppColors.darkColor,
+  );
+}
